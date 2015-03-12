5f2d76f42f0a1bbc21637782f19057f20e12c609
tepeka <tepeka@outlook.com>
2015-03-12 02:09:37 +0100

    add git commit hash to version service

--- inventory-web/src/main/java/net/tepeka/inventory/service/ApplicationVersion.java
+++ inventory-web/src/main/java/net/tepeka/inventory/service/ApplicationVersion.java
@@ -1,5 +1,6 @@
 package net.tepeka.inventory.service;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
@@ -19,6 +20,7 @@ public class ApplicationVersion {
 
     private final static String MANIFEST = "/META-INF/MANIFEST.MF";
     private final static String IMPL_VERSION = "Implementation-Version";
+    private final static String GIT_SHA1 = "git-SHA-1";
 
     @Context
     ServletContext servlet;
@@ -27,18 +29,14 @@ public class ApplicationVersion {
     @Produces("text/plain")
     public String getVersion(@QueryParam("pre") String pre) {
         try {
-            return (pre != null ? pre : "") + retrieveVersion();
+            InputStream inputStream = servlet.getResourceAsStream(MANIFEST);
+            Manifest manifest = new Manifest(inputStream);
+            Attributes attributes = manifest.getMainAttributes();
+            return (pre != null ? pre : "") + attributes.getValue(IMPL_VERSION) + " " + attributes.getValue(GIT_SHA1);
 
         } catch (Exception e) {
             return "manifest information is not available :- (";
         }
-    }
-
-    private String retrieveVersion() throws IOException {
-        InputStream inputStream = servlet.getResourceAsStream(MANIFEST);
-        Manifest manifest = new Manifest(inputStream);
-        Attributes atts = manifest.getMainAttributes();
-        return atts.getValue(IMPL_VERSION);
     }
 
 }
